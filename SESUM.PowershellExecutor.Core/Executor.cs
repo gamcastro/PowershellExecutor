9d22cc55da93b9666a3266744b8b19788055babe
George Castro <gamcastro@bol.com.br>
2020-09-18 07:48:44 -0300

    Adicionar arquivos de projeto.

---
+++ SESUM.PowershellExecutor.Core/Executor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SESUM.PowershellExecutor.Core
+{
+    public class Executor
+    {
+        public static void ExecutarScript(string scriptFile)
+        {
+            if (File.Exists(scriptFile))
+            {
+                string extensao = Path.GetExtension(scriptFile);
+                if (extensao == ".ps1")
+                {
+                    using (Process myProcess = new Process())
+                    {
+                        myProcess.StartInfo.UseShellExecute = false;
+                        myProcess.StartInfo.FileName = "powershell.exe";
+                        myProcess.StartInfo.Arguments = $"-ExecutionPolicy Bypass -File {scriptFile}";
+                        myProcess.Start();
+                    }
+                }
+                else
+                {
+                    throw new Exception("Não é um arquivo de script powershell");
+                }
+
+
+            }
+            else
+            {
+                throw new FileNotFoundException("Arquivo de script não encontrado", scriptFile);
+            }
+
+        }
+
+    }
+}
